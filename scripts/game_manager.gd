433765da6f0587aca10fea99cf69334ad0a3f0d8
veritasia <aminaapstudent@yahoo.com>
2024-09-10 08:50:33 -0700

    has changed collision things

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,11 +1,6 @@
 extends Node2D
 @onready var player: CharacterBody2D = $"../Player"
 
-func _ready() -> void:
-	print(player.health)
-	player.health -= 20
-	print(player.health)
-
 func fireDamage():
 	player.health -= 20
 	print(player.health)
